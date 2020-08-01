ddf46fa79ec64566b49e4eaec141b3454c309914
Gustavo Sabel <gustavo.sabel.gs@gmail.com>
2020-08-01 20:33:46 -0300

    Configurado docker

--- API/CopaMundoFilmes.Api/Startup.cs
+++ API/CopaMundoFilmes.Api/Startup.cs
@@ -2,12 +2,14 @@ using CopaMundoFilmes.Api.Extensions;
 using CopaMundoFilmes.Api.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Polly;
 using Polly.Extensions.Http;
 using System;
 using System.Net.Http;
+using System.Text;
 
 namespace CopaMundoFilmes.Api
 {
@@ -39,6 +41,16 @@ namespace CopaMundoFilmes.Api
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseExceptionHandlerApp(env);
+
+            app.Use(async (context, next) =>
+            {
+                await next();
+                if (context.Response.StatusCode == 404)
+                {
+                    context.Response.ContentType = "text/html";
+                    await context.Response.WriteAsync("<h1>Página não encontrada</h1>");
+                }
+            });
 
             app.UseCors(x =>
             {
